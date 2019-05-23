999968f8f50546c25c7252dbe168e0aac66f0b06
KEVINOH <aflashyrhetoric@gmail.com>
2019-05-23 16:26:48 -0400

    Add prettier and new tweaks to messenger

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// NOTE: Since backlog-com-website repo uses older JS, the prettier configuration here is DIFFERENT than in other repos
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+}
