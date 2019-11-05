1d91e369d2d3a5cf58a0c9c9e035fdd60d1e32ff
Kevin Oh <ko@Kevins-MacBook-Air.local>
2019-11-05 18:07:03 -0500

    Begin work on the overhaul of the Night Messenger to algorithmically generate theme colorization - TOOLING: Extracted some config out of gulpfile, proceed to ES6-ify everything
    TODO: Establish PURGECSS as well as javascript minification as part of the build processgs
    
    Author:    Kevin Oh <aflashyrhetoric@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
-'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./nightmode.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./*.scss', ['sass']);
-});
+'use strict'
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+
+const sass_config = { outputStyle: 'compressed' }
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./nightmode.scss')
+    .pipe(sass(sass_config).on('error', sass.logError))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./*.scss', ['sass'])
+})
