b0440375633c074de07567335e6d839e82db3751
Jason Brooks <cs.jasonbrooks@gmail.com>
2019-12-28 13:55:47 -0500

    Remove zip, add fixes, update to some modern js standards

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp ./background.js ./nightmessenger
+cp ./day.js ./nightmessenger
+cp ./driver.js ./nightmessenger
+cp ./manifest.json ./nightmessenger
+cp ./night.js ./nightmessenger
+cp ./nightmessenger128.png ./nightmessenger
+cp ./nightmessenger48.png ./nightmessenger
+cp ./nightmessenger16.png ./nightmessenger
+cp ./nightmode.css ./nightmessenger
+cp ./off.png ./nightmessenger
+cp ./on.png ./nightmessenger
